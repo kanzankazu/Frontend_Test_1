413d6686b359b4720c0aa2d6c2a2f27ce3fee30a
Faisal Bahri <kanzankazu46@gmail.com>
2024-02-22 01:14:35 +0700

    refactor code

--- core/src/main/java/com/danamon/core/util/RobotTyping.kt
+++ core/src/main/java/com/danamon/core/util/RobotTyping.kt
@@ -2,37 +2,8 @@ package com.danamon.core.util
 
 import android.os.Handler
 import android.os.Looper
-import android.widget.EditText
 import android.widget.TextView
 import com.danamon.core.ext.delayFun
-
-fun EditText.typing(
-    text: String,
-    isRepeatForever: Boolean = false,
-    delayMillis: Long = 100,
-    isNewTextAlwaysClear: Boolean = true,
-    isRepeatForeverAlwaysClear: Boolean = true,
-    onFinish: (() -> Unit)? = null,
-) {
-    if (isNewTextAlwaysClear) setText("")
-    val handler = Handler(Looper.getMainLooper())
-    val task: Runnable = object : Runnable {
-        override fun run() {
-            if (isRepeatForeverAlwaysClear) setText("")
-            text.forEachIndexed { index, c ->
-                handler.postDelayed({
-                    append(c.toString())
-                    if (index == text.lastIndex && !isRepeatForever) {
-                        context.delayFun { onFinish?.invoke() }
-                    }
-                }, index * delayMillis)
-            }
-            if (isRepeatForever) handler.postDelayed(this, text.length * delayMillis)
-        }
-    }
-
-    handler.post(task)
-}
 
 fun TextView.typing(
     text: String,
